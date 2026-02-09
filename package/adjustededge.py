4203df4ce9b00c67ccbebc4cd0d5483f72a801e2
archeasphere <apokemon35966@gmail.com>
2026-02-09 11:34:12 -0800

    Update Edge additio

--- package/adjustededge.py
+++ package/adjustededge.py
@@ -12,7 +12,25 @@ def find_lengths(color_array_1, color_array_2):
             lengths[(tuple(color1), tuple(color2))] = distance
     return lengths
 
-def new_edge(river_side_1, river_side_2):
+#Takes the coordiantes of the two river sides and the length to adjust by, and returns the new edge coordinates after adding the length in line with the nearest pixel on the opposite river side
+def new_edge(river_side_1, river_side_2, length):
+    new_edge = {}
+    #find the angle between the two river sides
+    for i in range(len(river_side_1)):
+        color1 = np.array(river_side_1[i])
+        nearest_color2 = None
+        nearest_distance = float('inf')
+        for j in range(len(river_side_2)):
+            color2 = np.array(river_side_2[j])
+            distance = np.sqrt(np.sum((color2 - color1) ** 2))
+            if distance < nearest_distance:
+                nearest_distance = distance
+                nearest_color2 = color2
+        #calculate the angle between the two colors
+        angle = np.arctan2(nearest_color2[1] - color1[1], nearest_color2[0] - color1[0])
+        #calculate the new edge coordinates by adding the length in line with the angle
+        new_x = int(color1[0] + length * np.cos(angle))
+        new_y = int(color1[1] + length * np.sin(angle))
+        new_edge[tuple(color1)] = (new_x, new_y)
     
-    new_edges = {}
     
